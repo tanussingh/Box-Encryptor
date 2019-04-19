29fb425767d309a9a531ac30c1dd9937d8a796f1
Tanushri Singh <tanussingh@gmail.com>
2019-04-19 15:46:30 -0500

    more functions added

--- Source Files/GUI.py
+++ Source Files/GUI.py
@@ -8,6 +8,7 @@
 ###################
 
 #Import Libaries
+from tkinter import font
 from tkinter import *
 
 class GUI:
@@ -54,38 +55,27 @@ class GUI:
         title = Label(rootWindow, text = "ENCRYPTBOX", bg = "black", fg = "white")
         title.config(font = ("verdana", 40))
         title.pack(fill=X)
-        optionTitle = Label(rootWindow, text = "What would you like to conduct today?", bg = "black", fg = "red")
+        optionTitle = Label(rootWindow, text = "Action of Choice?", bg = "black", fg = "red")
         optionTitle.config(font = ("verdana", 25))
         optionTitle.pack(fill=X)
 
         #Provide the 3 option buttons
-        submitAuth = Button(text = "Upload Files", fg = "black")
+        helv36 = font.Font(family='Helvetica', size=24)
+        submitAuth = Button(text = "Upload Files", fg = "black", font=helv36)
         submitAuth.place(height = 40, width = 250, x = 150, y = 100)
-        submitAuth = Button(text = "Download Files", fg = "black")
+        submitAuth = Button(text = "Download Files", fg = "black", font=helv36)
         submitAuth.place(height = 40, width = 250, x = 150, y = 175)
-        submitAuth = Button(text = "Share Files", fg = "black")
+        submitAuth = Button(text = "Share Files", fg = "black", font=helv36)
         submitAuth.place(height = 40, width = 250, x = 150, y = 250)
 
         rootWindow.mainloop()
 
-    def uploadFiles():
-        # Set up Options Frame
-        rootWindow = Tk()                   #Constructor rootWindow - main window
-        rootWindow.title('Upload')
-        rootWindow.geometry("550x320")
-        rootWindow.resizable(0,0)
-        title = Label(rootWindow, text = "ENCRYPTBOX", bg = "black", fg = "white")
-        title.config(font = ("verdana", 40))
-        title.pack(fill=X)
-        uploadTitle = Label(rootWindow, text = "Uploading Files", bg = "black", fg = "red")
-        uploadTitle.config(font = ("verdana", 25))
-        uploadTitle.pack(fill=X)
 
     def downloadFiles():
         # Set up Options Frame
         rootWindow = Tk()                   #Constructor rootWindow - main window
         rootWindow.title('Download')
-        rootWindow.geometry("550x320")
+        rootWindow.geometry("600x320")
         rootWindow.resizable(0,0)
         title = Label(rootWindow, text = "ENCRYPTBOX", bg = "black", fg = "white")
         title.config(font = ("verdana", 40))
@@ -94,18 +84,19 @@ class GUI:
         downloadTitle.config(font = ("verdana", 25))
         downloadTitle.pack(fill=X)
 
-    def shareFiles():
-        # Set up Options Frame
-        rootWindow = Tk()                   #Constructor rootWindow - main window
-        rootWindow.title('Share')
-        rootWindow.geometry("550x320")
-        rootWindow.resizable(0,0)
-        title = Label(rootWindow, text = "ENCRYPTBOX", bg = "black", fg = "white")
-        title.config(font = ("verdana", 40))
-        title.pack(fill=X)
-        shareTitle = Label(rootWindow, text = "Sharing Files", bg = "black", fg = "red")
-        shareTitle.config(font = ("verdana", 25))
-        shareTitle.pack(fill=X)
+        #Name of File that is to be downloaded
+        pathOfFile = Label(rootWindow, text = "Name of file: ", fg = "black")
+        pathOfFile.config(font = ("Arial", 20))
+        pathOfFile.place(x = 25, y = 100)
+        inputFileName = Entry(rootWindow)
+        inputFileName.place(height = 40, width = 300, x = 280, y = 100)
+        pathPublicKey = Label(rootWindow, text = "Path for Public Key File: ", fg = "black")
+        pathPublicKey.config(font = ("Arial", 20))
+        pathPublicKey.place(x= 25, y = 150)
+        inputPublicKey = Entry(rootWindow)
+        inputPublicKey.place(height = 40, width = 300, x = 280, y = 150)
+        rootWindow.mainloop()
+
 
     authWindow()
     optionWindow()
